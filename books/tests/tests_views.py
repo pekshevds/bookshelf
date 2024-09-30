caa10bbe14c464caf2e44be16edd4274e547b306
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-30 10:33:56 +0300

    testing views

--- books/tests/tests_views.py
+++ books/tests/tests_views.py
@@ -1,5 +1,71 @@
+import pytest
 from django.test import TestCase
+from django.urls import reverse
 from books.models import Book
+
+
+@pytest.fixture()
+def book():
+    return {
+        "title": "book1",
+        "author_full_name": "author1",
+        "year_of_publishing": 2019,
+        "copies_printed": 5000,
+        "short_description": "some description1",
+    }
+
+
+@pytest.mark.django_db
+def test__book_list_view__checks_route_books_list_is_calling(client):
+    url = reverse("books:books-list")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test__book_list_view__checks_finding_some_book_by_id_200(client, book):
+    new_book = Book.objects.create(**book)
+    url = reverse("books:book", kwargs={"book_id": new_book.id})
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test__book_list_view__checks_finding_some_book_by_id_404(client):
+    url = reverse("books:book", kwargs={"book_id": 1})
+    response = client.get(url)
+    assert response.status_code == 404
+
+
+@pytest.mark.django_db
+def test__book_list_view__checks_route_books_api_list_is_calling(client):
+    url = reverse("books:books-api-list")
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test__book_list_api_view__checks_the_result_contents_several_books(client, book):
+    Book.objects.create(**book)
+    url = reverse("books:books-api-list")
+    response = client.get(url)
+    data = response.json()
+    assert len(data) >= 1
+
+
+@pytest.mark.django_db
+def test__book_list_api_view__checks_finding_some_book_by_id_200(client, book):
+    new_book = Book.objects.create(**book)
+    url = reverse("books:api-book", kwargs={"book_id": new_book.id})
+    response = client.get(url)
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db
+def test__book_list_api_view__checks_finding_some_book_by_id_404(client, book):
+    url = reverse("books:api-book", kwargs={"book_id": 1})
+    response = client.get(url)
+    assert response.status_code == 404
 
 
 class TestViewsBook(TestCase):
