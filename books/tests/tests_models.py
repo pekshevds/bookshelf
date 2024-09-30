247020c9145168a46aa5ebcb28005ae0f8ed3b70
denis sergeevich pekshev <pekshev.ds@gmail.com>
2024-09-30 08:51:31 +0300

    test models

--- books/tests/tests_models.py
+++ books/tests/tests_models.py
@@ -1,12 +1,24 @@
 import pytest
 from django.test import TestCase
+from django.db.utils import IntegrityError
 from books.models import Book
 
 
+@pytest.fixture()
+def book():
+    return {
+        "title": "book1",
+        "author_full_name": "author1",
+        "year_of_publishing": 2019,
+        "copies_printed": 5000,
+        "short_description": "some description1",
+    }
+
+
 @pytest.mark.django_db
-def test_books_with_the_same_authors():
-    books_count = 5
-    author_full_name = "some author"
+def test_create_various_books_names_with_the_same_authors():
+    books_count = 2
+    author_full_name = "author1"
     for i in range(books_count):
         Book.objects.create(
             title=f"title{i}",
@@ -16,6 +28,28 @@ def test_books_with_the_same_authors():
             short_description="",
         )
     assert Book.objects.filter(author_full_name=author_full_name).count() == books_count
+
+
+@pytest.mark.django_db
+def test_create_the_same_books_names_with_various_authors():
+    books_count = 2
+    title = "book1"
+    for i in range(books_count):
+        Book.objects.create(
+            title=title,
+            author_full_name=f"author{i}",
+            year_of_publishing=2020,
+            copies_printed=200,
+            short_description="",
+        )
+    assert Book.objects.filter(title=title).count() == books_count
+
+
+@pytest.mark.django_db
+def test_tryes_to_create_the_same_books_with_one_author(book):
+    Book.objects.create(**book)
+    with pytest.raises(IntegrityError):
+        Book.objects.create(**book)
 
 
 class TestBook(TestCase):
